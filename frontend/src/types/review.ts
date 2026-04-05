f6891ea1ec5427715cb11c92ea5c6c45ac0c9276
Quoc Anh <quocanhlove199@gmail.com>
2026-04-05 07:24:37 +0700

    feat: type feature

---
+++ frontend/src/types/review.ts
@@ -0,0 +1,8 @@
+export interface IReview {
+  _id?: string;
+  name: string; // hoặc có thể là { name: string } tùy backend trả về
+  rating: number;
+  comment: string;
+  createdAt: string;
+  user: string;
+}
